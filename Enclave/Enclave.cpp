1952bce7ae9dfa60291556f14db9f509e34423e8
root <satyakisen2012@gmail.com>
2019-10-09 04:17:32 +0000

    commit

---
+++ Enclave/Enclave.cpp
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2011-2018 Intel Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+#include <stdarg.h>
+#include <stdio.h>      /* vsnprintf */
+
+#include "Enclave.h"
+#include "Enclave_t.h"  /* print_string */
+
+/* 
+ * printf: 
+ *   Invokes OCALL to display the enclave buffer to the terminal.
+ */
+void MatrixChainOrder() 
+{ 
+  
+    /* For simplicity of the program, one  
+    extra row and one extra column are  
+    allocated in m[][]. 0th row and 0th 
+    column of m[][] are not used */
+    
+    int p[] = {40,20,30,10,30}; 
+    int n = sizeof(p) / sizeof(p[0]); 
+
+    int m[n][n]; 
+  
+    int i, j, k, L, q; 
+  
+    /* m[i,j] = Minimum number of scalar  
+    multiplications needed to compute the 
+    matrix A[i]A[i+1]...A[j] = A[i..j] where 
+    dimension of A[i] is p[i-1] x p[i] */
+  
+    // cost is zero when multiplying 
+    // one matrix. 
+    for (i = 1; i < n; i++) 
+        m[i][i] = 0; 
+  
+    // L is chain length. 
+    for (L = 2; L < n; L++) 
+    { 
+        for (i = 1; i < n - L + 1; i++) 
+        { 
+            j = i + L - 1; 
+            m[i][j] = 1000000; 
+            for (k = i; k <= j - 1; k++) 
+            { 
+                // q = cost/scalar multiplications 
+                q = m[i][k] + m[k + 1][j] +  
+                    p[i - 1] * p[k] * p[j]; 
+                if (q < m[i][j]) 
+                    m[i][j] = q; 
+            } 
+        } 
+    }
+    char buf[100];
+    snprintf(buf, 100,"%d\n", m[1][n-1]);
+   
+    ocall_print_string(buf); 
+  
+} 
+
+
